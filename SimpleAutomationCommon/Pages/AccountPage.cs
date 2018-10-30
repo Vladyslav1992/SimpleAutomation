c15d747eeebcb1f1a275758e7d1620bd54ed5ede
Sergey Sila <sila.sergey89@gmail.com>
2018-10-30 09:43:46 +0200

    bla-bla

--- SimpleAutomationCommon/Pages/AccountPage.cs
+++ SimpleAutomationCommon/Pages/AccountPage.cs
@@ -7,13 +7,14 @@ namespace SimpleAutomationCommon.Pages
 {
     using _ = AccountPage;
 
+    [Url("user")]
     public class AccountPage : Page<_>
     {
         [FindByXPath("//a[text()='Edit']")] 
         private Label<_> EditAccount { get; set; }
 
-        [FindByXPath("//div[.='Account Information']/following-sibling::div")]
-        private Label<_> UserInfo { get; set; }
+        [FindByXPath("//div[.='Account Information']/following-sibling::div")] ////div[.='Account Information']/following-sibling::div)/descendant-or-self::label
+        private Text<_> UserInfo { get; set; }
 
         public IEnumerable<string> GetInfo()
             => UserInfo.Get().Remove("\r\nEdit", "\r").Split('\n').Select(i => i.Trim());
---
+++ SimpleAutomationTests/Tests/Login.cs
@@ -0,0 +1,23 @@
+﻿using Atata;
+using NUnit.Framework;
+using SimpleAutomationCommon.DataModels.Builders;
+using SimpleAutomationCommon.Pages;
+using SimpleAutomationCommon.Pages.LoginPg;
+
+namespace SimpleAutomationTests.Tests
+{
+    public class Login : BaseTest
+    {
+        [Test]
+        public void Test()
+        {
+            var loginPage = Go.To<LoginPage>();
+            loginPage.FillAndSubmit(BaseUsers.GetUser("commonUser"));
+            var accountPage = Go.To<AccountPage>();
+            accountPage.GetInfo();
+        }
+
+
+    }
+    
+}
